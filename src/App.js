1ee516c55774d20f79e88fe12bf9acc7b260a831
guille garcia <ggarciacardiel@gmail.com>
2023-03-03 10:52:05 +0100

    it4 adds AddFoodForm

--- src/App.js
+++ src/App.js
@@ -3,12 +3,22 @@ import React, { useState } from 'react';
 import { Row, Divider, Button } from 'antd';
 import foodsData from "./foods.json";
 import FoodBox from './components/FoodBox';
+import AddFoodForm from './components/AddFoodForm';
 
 function App() {
-  const [foods, setFoods] = useState(foodsData);
-
+  const [foods, setFoods] = useState(foodsData); 
+  
+  const handleNewFood = (food) => {
+    const newFood = {...food, _id: foods.length + 1}
+    setFoods([...foods, newFood])
+  }
+  
   return (
     <div className="App">
+    <Divider>Add Food Entry</Divider>
+    <Row style={{ width: '100%', justifyContent: 'center' }}>
+      <AddFoodForm handleNewFood={handleNewFood}/>
+    </Row>
     <Divider>Food List</Divider>
     <Row style={{ width: '100%', justifyContent: 'center' }}>
       {foods.map(food => {
